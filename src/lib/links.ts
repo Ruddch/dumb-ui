b2f0bb97a8eda6234fff2c712917e26b47cee060
Semion Shishlov <semionshish@gmail.com>
2026-07-22 03:42:55 +0300

    fix

--- src/lib/links.ts
+++ src/lib/links.ts
@@ -1,5 +1,3 @@
-import { env } from '../config/env';
-
 export const TWITTER_URL = 'https://x.com/DumbMoneyGroup';
 
 const POSM =
@@ -7,7 +5,7 @@ const POSM =
   '0x58daec3116aae6D93017bAAea7749052E8a04fA7';
 
 export function uniswapV4PositionUrl(tokenId: bigint): string {
-  return `https://app.uniswap.org/positions/v4/${env.chainId}/${tokenId.toString()}`;
+  return `https://app.uniswap.org/positions/v4/robinhood/${tokenId.toString()}`;
 }
 
 export function positionNftExplorerUrl(tokenId: bigint): string {
